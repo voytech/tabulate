c685bcdfd3bf08c47efaa4644127f77a6cd815dc
voytech <voytech.m@gmail.com>
2020-06-02 21:50:01 +0200

    Under the hood delegating of tableExtensions to set of table extension operation interface implementors (extension managing abstraction)

--- src/main/kotlin/pl/voytech/exporter/core/model/Table.kt
+++ src/main/kotlin/pl/voytech/exporter/core/model/Table.kt
@@ -6,18 +6,20 @@ import pl.voytech.exporter.core.model.extension.TableExtension
 /**
  * A top-level model class. Defines how records from repositories will be handled by file rendering strategies.
  *
- * 'hints' is special field which opens model for extensions. It holds a list of 'custom attributes' for enriching
+ * special property class - '...extensions' holds a list of 'custom attributes' for enriching
  * capabilities of exporters for different output formats.
- * It has been called 'hints' rather than 'customAttributes' because it should be possible to:
- * - always export data without any hints applied using each implemented exporter,
- * - custom attributes (hints) reserved for particular exporter does not restricts model for being exported only by this
+ * It has been called 'extensions' rather than 'customAttributes' because it should be possible to:
+ * - always export data without any extensions applied using each implemented exporter,
+ * - custom attributes reserved for particular exporter does not restricts model for being exported only by this
  *   particular exporter,
  * - effectively any model with any custom attribute should be compatible with any exporter,
- * - 'hint' seems to better describe the model entity than 'custom attribute' as custom attribute seems to have the same
+ * - 'extension' seems to better describe the model entity than 'custom attribute' as custom attribute seems to have the same
  *   priority as canonical attributes (fields of Table class). Also 'custom attribute' seems to be applicable by all
- *   exporters in the same manner (it is custom only because it is unknown at compile time e.g.). Hint on the other hand
- *   is just a 'suggestion' which may not be applicable for exporter, but should not break compatibility between model
+ *   exporters in the same manner (it is custom only because it is unknown at compile time e.g.). extension on the other hand
+ *   is just a 'suggestion' which may not be applicable for exporter in all available contexts, but this fact should not break compatibility between model
  *   and exporter.
+ *
+ *   @author Wojciech Mąka
  */
 data class Table<T>(
     val name: String? = "untitled",
