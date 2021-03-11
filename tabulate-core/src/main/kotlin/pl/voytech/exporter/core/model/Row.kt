774d4c912349f8b17e58832b69bd54c32585dcee
voytech <voytech.m@gmail.com>
2021-03-11 20:34:49 +0100

    add utility model extension functions

--- tabulate-core/src/main/kotlin/pl/voytech/exporter/core/model/Row.kt
+++ tabulate-core/src/main/kotlin/pl/voytech/exporter/core/model/Row.kt
@@ -1,7 +1,7 @@
 package pl.voytech.exporter.core.model
 
-import pl.voytech.exporter.core.model.attributes.alias.CellAttribute
 import pl.voytech.exporter.core.model.attributes.RowAttribute
+import pl.voytech.exporter.core.model.attributes.alias.CellAttribute
 
 data class Row<T> internal constructor(
     val selector: RowSelector<T>? = null,
@@ -11,3 +11,14 @@ data class Row<T> internal constructor(
     val cells: Map<ColumnKey<T>, Cell<T>>?
 )
 
+fun <T> Map<ColumnKey<T>, Cell<T>>?.resolveCellValue(key: ColumnKey<T>, maybeRow: SourceRow<T>? = null): Any? {
+    return this?.get(key)?.let { cell ->
+        maybeRow?.let { row ->
+            cell.eval?.invoke(row)
+                ?: cell.value
+                ?: row.record?.let { record -> key.ref?.invoke(record) }
+        } ?: cell.value
+    } ?: maybeRow?.let { row ->
+        row.record?.let { record -> key.ref?.invoke(record) }
+    }
+}
