79ab94d3243d256fd8781cf583bd4f0fa9906a47
voytech.m <voytech.m@gmail.com>
2021-04-19 20:29:50 +0200

    SPI support through Java ServiceLoader #81

--- tabulate-excel/src/main/kotlin/pl/voytech/exporter/impl/template/excel/PoiExcelTabulateProvider.kt
+++ tabulate-excel/src/main/kotlin/pl/voytech/exporter/impl/template/excel/PoiExcelTabulateProvider.kt
@@ -2,12 +2,13 @@ package pl.voytech.exporter.impl.template.excel
 
 import pl.voytech.exporter.core.template.operations.ExportOperationConfiguringFactory
 import pl.voytech.exporter.core.template.spi.ExportOperationFactoryProvider
+import pl.voytech.exporter.core.template.spi.Identifiable
 
 class PoiExcelTabulateProvider : ExportOperationFactoryProvider {
 
     override fun <T> create(): ExportOperationConfiguringFactory<T> = PoiExcelExportOperationsFactory()
 
-    override fun id(): String = ID
+    override fun test(t: Identifiable): Boolean = ID == t.getIdent()
 
     companion object {
         const val ID = "xlsx"
